711f97d75b71933dd70b6217e2117ab60dcd23b4
Natalie Baker <nathelene76@gmail.com>
2023-08-25 10:53:12 -0600

    add nav component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+ const Nav = () => {
+
+    return (
+        <nav className='nav'>
+            <h2 className='welcome-user'></h2>
+            <div className='calendar'></div>
+
+        </nav>
+    )
+}
+
+export default Nav
