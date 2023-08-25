542aee1d102214cff2fe848c2dc623bcffb3d9e4
Caitlin Cradick <caitlincradick@gmail.com>
2023-08-25 16:54:45 -0700

    add data from mock data to render on cards

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './Cards.css'
 
-const Cards = () => {
+const Cards = ({name, entry, date}) => {
   return (
     <div className='cards'>
     <div className='title'>
-      <h3>Date: </h3>
-      <h3>Name: </h3>
+      <h3>Date: {date} </h3>
+      <h3>Name: {name} </h3>
     </div>  
     <div className='win'>
-      <p>win</p>
+      <p>{entry}</p>
       </div>
     </div>
   )
