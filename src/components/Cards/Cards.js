a43cc669fbd66d5391eebe6900f8d7e961a4ff71
Caitlin Cradick <caitlincradick@gmail.com>
2023-08-25 15:02:15 -0700

    add basic set up t o card component

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import './Cards.css'
 
 const Cards = () => {
   return (
-    <div>Cards</div>
+    <div className='cards'>
+    <div className='title'>
+      <h3>Date: </h3>
+      <h3>Name: </h3>
+    </div>  
+    <div className='win'>
+      <p>win</p>
+      </div>
+    </div>
   )
 }
 
