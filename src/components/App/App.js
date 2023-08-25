eca257f7a1452ce26e0370de59726e3e34f7ea77
Caitlin Cradick <caitlincradick@gmail.com>
2023-08-25 16:53:54 -0700

    add state to wins and pass down in entries container

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,16 +4,21 @@ import Datepage from '../Datepage/Datepage'
 import NotFound from '../NotFound/NotFound'
 import Nav from "../NavBar/NavBar.js";
 import Cards from "../Cards/Cards.js"
+import EntriesContainer from "../EntriesContainer/EntriesContainer"
+import cardData from '../../cardMockData';
 import React from 'react'
+import { useState } from 'react';
 import { Routes, Route} from 'react-router-dom'
 // import { useEffect, useState } from 'react';
 
 
 const App = () => {
+const [wins, setWins] = useState(cardData)
+
   return (
     <div>
       <Nav />
-      <Cards />
+      <EntriesContainer wins = {wins}/>
       <Routes>
         <Route exact path='/' element={<Homepage />} />
         <Route path='date/:date' element={<Datepage/>} />
