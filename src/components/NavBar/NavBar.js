ff1b0cca4036d0f565ad76513d18f4e65de33b17
Natalie Baker <nathelene76@gmail.com>
2023-08-25 12:22:22 -0600

    add header to nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,7 +2,7 @@
 
     return (
         <nav className='nav'>
-            <h2 className='welcome-user'></h2>
+            <h2 className='welcome-user'>Welcome User!</h2>
             <div className='calendar'>
                 <input type='date'/>
             </div>
