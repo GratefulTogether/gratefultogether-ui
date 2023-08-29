29dc36cc34b3e6b0152b56743f0b1eae8911303e
Natalie Baker <nathelene76@gmail.com>
2023-08-29 13:22:42 -0600

    make logo link to home

--- src/components/Datepage/Datepage.js
+++ src/components/Datepage/Datepage.js
@@ -2,13 +2,16 @@ import './Datepage.css'
 import Homelogo from '../../images/Homelogo.png'
 import Nav from "../NavBar/NavBar.js";
 import EntriesContainer from '../EntriesContainer/EntriesContainer';
+import { NavLink } from 'react-router-dom';
 
 const Datepage = ({date, setDate, wins, setWins}) => {
   return (
     <>
       <Nav />
       <div className='date-page-container'>
+        <NavLink to = "/">
         <img className='home-logo' src={Homelogo} alt="logo with open book" />
+        </NavLink>
         <h1>Entries for {date}:</h1>
       <EntriesContainer wins={wins} setWins={setWins} />
       </div>
