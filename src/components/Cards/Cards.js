385e885896268a3c554ba9780aa331e1087c73d5
Calli Herrmann <calliherrmann@gmail.com>
2023-08-29 17:39:13 -0400

    Add notecard images and fonts

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,30 +1,45 @@
 import React from 'react';
 import styled from 'styled-components';
+import green from '../../images/notecards/green.png';
+import lightGreen from '../../images/notecards/light-green.png';
+import purple from '../../images/notecards/purple.png';
+import tanCircle from '../../images/notecards/orange.png';
+import tanSquare from '../../images/notecards/orange1.png';
+import yellowSq from '../../images/notecards/orange2.png';
+import yellow from '../../images/notecards/yellow.png';
+
+const notecards = [green, lightGreen, purple, tanCircle, tanSquare, yellow, yellowSq]
+const randomIndex = Math.floor(Math.random() * notecards.length)
+let currentBackground = notecards[randomIndex]
 
 const Card = styled.div`
-  box-sizing: border-box;
-  border: 3px #00717F;
-  border-style: groove;
+  background: url(${currentBackground});
   padding: 10px;  
-  height: 20vh;
-  width: 40vw;
+  height: 30vh;
+  width: 26vw;
+  transition: 1s;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
   transition: 1s;
   &:hover {
     transform: scale(1.4);
-    margin: 4.5%
+    margin: 5.5%
   }
-
 `;
 
 const TitleOfCard = styled.div`
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  margin: 10% 0% 0% 10%;
+  font-family: 'Indie Flower', cursive;
 `;
 
 const WinOfCard = styled.div`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+  font-family: 'Homemade Apple', cursive;
 `;
 
 const Cards = ({name, entry, date}) => {
