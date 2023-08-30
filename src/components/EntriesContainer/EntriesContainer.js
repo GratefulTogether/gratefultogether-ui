1bd3c6bed2e78a680afd3efd6a0376bb13f9904e
Natalie Baker <nathelene76@gmail.com>
2023-08-30 17:35:02 -0600

    fix circleci error

--- src/components/EntriesContainer/EntriesContainer.js
+++ src/components/EntriesContainer/EntriesContainer.js
@@ -2,8 +2,7 @@ import React from 'react'
 import Cards from '../Cards/Cards'
 import styled from 'styled-components';
 import './EntriesContainer.css'
-import CalendarModal from '../CalendarModal/CalendarModal';
-import { useState } from 'react';
+
 
 const CardContainer = styled.div`
   display: flex;
@@ -25,21 +24,7 @@ const EntriesContainer = ({wins, date, setDate}) => {
     return notecards[Math.floor(Math.random() * notecards.length)];
   };
 
-  // const [isModalOpen, setIsModalOpen] = useState(false);
 
-  // const handleClick = () => {
-  //   setIsModalOpen(true);
-  // };
-
-  // const closeModal = () => {
-  //   setIsModalOpen(false);
-  // };
-
-  // const renderModal = () => {
-	// 	return (
-	// 		<CalendarModal closeModal={closeModal} date={date} setDate={setDate}/>
-	// 	)
-	// }
 
   const singleWin = wins.map(win => {
     return (
@@ -51,11 +36,6 @@ const EntriesContainer = ({wins, date, setDate}) => {
 
   return (
     <div className='today-info-container'>
-      <div className='today-info'>
-        {/* {date && <h2>Today's Entries:</h2>}
-        <button onClick={handleClick}>View Past Entry</button> */}
-        {/* {isModalOpen && renderModal()} */}
-      </div>
       <EContainer>
         {wins.length ? singleWin : <p>Nothing recorded yet today!</p>} 
       </EContainer>
