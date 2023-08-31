2a6ae3a1a70495bfbc06d53c95a07ed00ff0d44d
Victor Sriqui <victorsriqui@gmail.com>
2023-08-30 21:48:42 -0600

    feature: Nice formatting for date banner, uselocation to prevent buggines.

--- src/components/Datepage/Datepage.js
+++ src/components/Datepage/Datepage.js
@@ -3,8 +3,23 @@ import Homelogo from '../../images/Homelogo.png'
 import Nav from "../NavBar/NavBar.js";
 import EntriesContainer from '../EntriesContainer/EntriesContainer';
 import { NavLink } from 'react-router-dom';
+import dayjs from 'dayjs'
+import {useLocation} from 'react-router-dom'
 
 const Datepage = ({date, setDate, wins, setWins}) => {
+
+  
+  const path = useLocation()
+  const {pathname} = path
+  const urlDate = pathname.slice(pathname.length - 10, pathname.length)
+
+ 
+
+  const bannerDate = dayjs(urlDate);
+  const formattedDate = bannerDate.format('MMMM D YYYY');
+
+
+
   return (
     <>
       <Nav />
@@ -12,7 +27,7 @@ const Datepage = ({date, setDate, wins, setWins}) => {
         <NavLink to = "/">
         <img className='home-logo' src={Homelogo} alt="logo with open book" />
         </NavLink>
-        <h1>Entries for {date}:</h1>
+        <h1>Entries for {formattedDate}:</h1>
       <EntriesContainer wins={wins} date={date} setDate={setDate}/>
       </div>
     </>
