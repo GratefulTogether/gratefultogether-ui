fcf2aaa72057d055be2e68ba3654a33e2018db70
Victor Sriqui <victorsriqui@gmail.com>
2023-08-30 21:59:48 -0600

    feature: 404 date page when user tampers with url and inputs bad date

--- src/components/Datepage/Datepage.js
+++ src/components/Datepage/Datepage.js
@@ -4,22 +4,28 @@ import Nav from "../NavBar/NavBar.js";
 import EntriesContainer from '../EntriesContainer/EntriesContainer';
 import { NavLink } from 'react-router-dom';
 import dayjs from 'dayjs'
-import {useLocation} from 'react-router-dom'
+import {useLocation, useNavigate} from 'react-router-dom'
+import { useEffect } from 'react';
 
 const Datepage = ({date, setDate, wins, setWins}) => {
 
+  const navigate = useNavigate();
   
   const path = useLocation()
   const {pathname} = path
   const urlDate = pathname.slice(pathname.length - 10, pathname.length)
-
- 
-
   const bannerDate = dayjs(urlDate);
   const formattedDate = bannerDate.format('MMMM D YYYY');
 
+ 
+    useEffect(() => {
+      if (formattedDate === 'Invalid Date') {
+        navigate("*");
+      }
+    }, [navigate, formattedDate])
 
 
+  if (formattedDate !== 'Invalid Date') {
   return (
     <>
       <Nav />
@@ -32,6 +38,7 @@ const Datepage = ({date, setDate, wins, setWins}) => {
       </div>
     </>
   )
+  } 
 }
 
 export default Datepage
