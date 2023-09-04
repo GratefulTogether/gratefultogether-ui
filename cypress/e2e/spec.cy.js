7480b2ce9977c414ba03bc8ff978a7bc550a63f3
Victor Sriqui <victorsriqui@gmail.com>
2023-09-03 20:36:39 -0600

    test: Testing the container that has the modal plus subheader, and the cards for proper elements and attributes.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,7 +3,7 @@ import dayjs from 'dayjs'
 const today = dayjs().format('YYYY-MM-DD')
 const todayStyled = dayjs().format('MMMM D, YYYY')
 
-describe('template spec', () => {
+describe('Main page elements and attributes.', () => {
 
   beforeEach (() => {
 
@@ -114,6 +114,110 @@ describe('template spec', () => {
           .should('eq', 'submit')
         })
         
+      })
+    })
+  })
+
+  it('Should mount with info element, and modal button. With proper elements and attr.', () => {
+
+    
+    cy.visit("http://localhost:3000/", {
+      onBeforeLoad(win) {
+
+      cy.stub(win, "WebSocket")
+
+      }
+    })
+    cy.wait('@initialGet')
+
+    cy.url()
+    .should('eq', 'http://localhost:3000/')
+
+    cy.get('.App')
+    .within(()=>{
+      cy.get('div')
+      .eq(0)
+      .within(()=>{
+        cy.get('.today-info')
+        .within(()=>{
+          cy.get('h2')
+          .should('have.text', 'Today\'s Entries:')
+          cy.get('button')
+          .should('have.text', 'View Past Entry')
+        })
+      })
+    })
+  })
+
+  it('Cards should display with proper elements, text, and attr.', () => {
+
+    
+    cy.visit("http://localhost:3000/", {
+      onBeforeLoad(win) {
+
+      cy.stub(win, "WebSocket")
+
+      }
+    })
+    cy.wait('@initialGet')
+
+    cy.url()
+    .should('eq', 'http://localhost:3000/')
+
+    cy.get('.App')
+    .within(()=>{
+      cy.get('div')
+      .first()
+      .within(()=>{
+        cy.get('.today-info-container')
+        .within(()=>{
+          cy.get('div')
+          .first()
+          .within(()=>{
+            cy.get(':nth-child(1) > div')
+            .first()
+              cy.get('div')
+              .first()
+              .within(()=>{
+                cy.get('div')
+                .first()
+                .within(()=>{
+                  cy.get('h3')
+                  .eq(0)
+                  .should('have.text', 'Date: September 3, 2023 ')
+                  cy.get('h3')
+                  .eq(1)
+                  .should('have.text', 'Name: Neato ')
+                })
+                cy.get('div')
+                .last()
+                .within(()=>{
+                  cy.get('p')
+                  .should('have.text', 'slept well')
+                })
+              })
+
+            cy.get(':nth-child(3) > div')
+            .within(()=>{
+              cy.get('div')
+              .eq(0)
+              .within(()=>{
+                cy.get('h3')
+                .first()
+                .should('have.text', 'Date: September 3, 2023 ')
+                cy.get('h3')
+                .last()
+                .should('have.text', 'Name: Circe ')
+              })
+              cy.get('div')
+                .last()
+                .within(()=>{
+                  cy.get('p')
+                  .should('have.text', 'Ate some cookies')
+                })
+            })
+          })
+        })
       })
     })
   })
